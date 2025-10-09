80fdd9fe2ec52075dbbf24a82b869810de9fbaab
Mark Yoxall <mark.yoxall65@hotmail.com>
2025-10-09 15:47:44 +0100

    Added fluent UI blazor web app

---
+++ CleanCut.FluentBlazorWebApp/CleanCut.FluentBlazorWebApp/Program.cs
@@ -0,0 +1,49 @@
+using CleanCut.FluentBlazorWebApp.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace CleanCut.FluentBlazorWebApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+        
+        // Add FluentUI Components and Services
+        builder.Services.AddFluentUIComponents();
+
+        // Add HttpClient for API calls
+        builder.Services.AddHttpClient();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        app.Run();
+    }
+}
